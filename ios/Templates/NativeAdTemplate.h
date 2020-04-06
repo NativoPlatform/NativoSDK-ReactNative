49f8a06c10cc1adf6ca8ac965134602ba72bff76
Matthew Murray <mattm.trinsic@gmail.com>
2020-04-05 18:48:12 -0700

    Support for iOS adChoices icons SDK-746

--- ios/Templates/NativeAdTemplate.h
+++ ios/Templates/NativeAdTemplate.h
@@ -14,6 +14,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @property (nonatomic, readonly) UIImageView *adImageView;
 @property (nonatomic, readonly) UIImageView *authorImageView;
+@property (nonatomic, readonly) UIView *adChoicesIconView;
 
 @end
 
